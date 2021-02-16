0110da341c2dcc1ae2da9cd8098d2d2fdbc9050d
Daniel Soares <danielsoaaraujo@hotmail.com>
2021-02-16 16:15:13 -0300

    Recebendo dados do banco na página home

--- models/Pedido.js
+++ models/Pedido.js
@@ -8,8 +8,8 @@ const Pedido = function(data, username, endereco) {
 }
 
 Pedido.prototype.create = function() {
-    const query = 'INSERT INTO pedidos(nome_destinatario, telefone_destinatario, observacoes, data_pedido, nome_entregador, status_pedido) values($1, $2, $3, $4, $5, 0)';
-    const values = [this.data.nome_destinatario, this.data.telefone_destinatario, this.data.observacoes, this.data.data_pedido, this.data.nome_entregador];
+    const query = 'INSERT INTO pedidos(nome_destinatario, telefone_destinatario, observacoes, data_pedido, nome_entregador, status_pedido, logradouro, numero_logradouro, bairro, complemento ) values($1, $2, $3, $4, $5, 0, $6, $7, $8, $9)';
+    const values = [this.data.nome_destinatario, this.data.telefone_destinatario, this.data.observacoes, this.data.data_pedido, this.data.nome_entregador, this.data.logradouro, this.data.numero_logradouro, this.data.bairro, this.data.complemento];
     return new Promise((resolve, reject) => {
         pool.query(query, values, (error, results) => {
             if (error) {
@@ -37,5 +37,13 @@ Pedido.prototype.countTotal = async function({ status } = {}) {
         .then(res => res.rows[0].count)
         .catch(e => console.error(e.stack))
 };
+
+Pedido.prototype.findLatest = async function() {
+    const query = 'select * from pedidos order by id_pedido desc limit 5'
+    return pool
+        .query(query)
+        .then(res => res.rows)
+        .catch(e => console.error(e.stack))
+}
 
 module.exports = Pedido;
